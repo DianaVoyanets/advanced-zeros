9602842cd8daafee129acb80676c2f3ea44171e7
d.voyanets <diana.voyanets@gmail.com>
2018-02-25 00:17:12 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+    var currBase = base,
+        result = Number.MAX_VALUE;
+
+    var getFactZeros = (base) => {
+        var count = 0,
+            j = Math.floor(number / base);
+        
+        while (j > 0) {
+            count += j;
+            j = Math.floor(j / base);
+        }
+        
+        return count;
+    };
+
+    var getBaseZeros = (base) => {
+        var count = 0;
+
+        while (currBase % base === 0) {
+            currBase = currBase / base;     
+            count++;
+        }
+        
+        return count;
+    };
+
+    for (var i = 2; i <= base; i++) {
+        if (currBase % i !== 0) continue;
+
+        var baseZeros = getBaseZeros(i),
+            factZeros = getFactZeros(i);
+
+        result = Math.min(Math.floor(factZeros / baseZeros), result);
+    }
+
+    return result;
 }
